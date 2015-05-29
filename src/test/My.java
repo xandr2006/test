f7863bfb86d942ada37920ffa41463c11293ff92
xandr <xandr2006@mail.ru>
2015-05-29 15:57:34 +0400

    add jdbc

--- src/test/My.java
+++ src/test/My.java
@@ -2,8 +2,13 @@ package test;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.sql.*;
 
 public class My {
+        static final String host = "jdbc:odbc:Driver={Microsoft Access Driver (*.mdb)}; DBQ=c:\\base.mdb;";
+        static final String user = "postgres";
+        static final String pass = "22091985";
+
         public static String RS() throws Exception
         {
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -22,6 +27,23 @@ public class My {
         }
         public static String test()
         {
+            try (
+                    Connection conn = DriverManager.getConnection(host, user, pass);
+                    Statement stmt = conn.createStatement();
+                    ResultSet rs = stmt.executeQuery("select * from clients")
+      /* stmt.executeUpdate for insert, update or delete */
+            ) {
+                while(rs.next()) {
+                    //int id = rs.getInt("id");
+                    String client = rs.getString("Client");
+                    //String phone = rs.getString("phone");
+                    //Date changed = rs.getDate("last_changed");
+
+                    System.out.println(client);
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
             return "test ok";
         }
     }
